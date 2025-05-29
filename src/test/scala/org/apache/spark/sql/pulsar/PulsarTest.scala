9dcfb5452d72ae15b391400fbb96c7b14ae0aa34
Zikang Han <zikang.han@databricks.com>
2025-05-29 23:01:13 +0000

    fix PulsarTest

--- src/test/scala/org/apache/spark/sql/pulsar/PulsarTest.scala
+++ src/test/scala/org/apache/spark/sql/pulsar/PulsarTest.scala
@@ -49,7 +49,7 @@ trait PulsarTest extends BeforeAndAfterAll with BeforeAndAfterEach {
   self: SparkFunSuite =>
   import PulsarOptions._
 
-  val CURRENT_VERSION = "3.3.2"
+  val CURRENT_VERSION = "4.0.5"
 
   var pulsarContainer: PulsarContainer = null
   var serviceUrl: String = null
